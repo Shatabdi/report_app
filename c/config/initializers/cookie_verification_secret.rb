e20bd1f695f4f259f73523ca9685107ad41a2296
Shatabdi <shatabdi.mohapatra@mindfiresolutions.com>
2014-01-20 11:35:09 +0530

    deleted unnecessary folders

--- c/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'bf4a6151e71e6e62cc46bd4bcf82041627e7b1790ac83e9f9280210d60972dfe6fc9f705bf6259644d5b2911c222ead5567d80bbedd10390e273e649ec40aa92';
--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '047fc615f9160acd8cd6fec17462065bcdaf0cd910f69471088c9f4c41e45df3aa3c13efb1a9823c0064f7ba70e3a03ab844fc5c3e521821fd4f2b87f5eac5e8';
